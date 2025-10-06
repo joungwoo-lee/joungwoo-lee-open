9f09f4e574a6e3402c63683f6c06e50f176d00ab
joungwoo-lee <129725465+joungwoo-lee@users.noreply.github.com>
2025-10-06 23:21:31 +0900

    Update autorun.sh 충돌 방지 스크립트 자동 실행
    # 충돌방지
    RUN python -m pip show google && python -m pip uninstall -y google || true \
        python -m pip install -U pip setuptools wheel \
        python -m pip install -U google-adk mcp

--- autorun.sh
+++ autorun.sh
@@ -1,1 +1,6 @@
+# 충돌방지
+RUN python -m pip show google && python -m pip uninstall -y google || true \
+    python -m pip install -U pip setuptools wheel \
+    python -m pip install -U google-adk mcp 
+
 print("도커 시작.")
